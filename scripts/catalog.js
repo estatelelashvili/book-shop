b99d0477e9307df3dc31774302e0da34576060a1
Estate Lelashvili <estatelelashvili@gmail.com>
2022-11-09 20:16:50 +0400

    feat: add books.json

--- scripts/catalog.js
+++ scripts/catalog.js
@@ -1,22 +1,22 @@
-// fetch('./books.json')
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     appendData(data);
-//   })
-//   .catch(function (err) {
-//     console.log('error: ' + err);
-//   });
+fetch('../assets/books.json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log('error: ' + err);
+  });
 
-function appendData() {
+function appendData(data) {
   let fragment = new DocumentFragment();
 
   const hello = document.createElement('h1');
 
-  hello.innerText = 'Hello Books!';
+  hello.innerHTML = 'Hello Books!';
 
-  fragment.append(hello);
+  fragment.appendChild(hello);
 
   document.body.appendChild(fragment);
 }
